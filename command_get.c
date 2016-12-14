8a69e545589e1aa639a8d4ae448c30c44f7303c7
Olivier Chatry <olivier@fieldap.com>
2016-12-14 21:53:12 +0100

    fixing command parsing to handle EOF

--- command_get.c
+++ command_get.c
@@ -7,11 +7,11 @@
 
 static int command_wait(shell_t *shell, int fd) {
 	fd_set rds;
-	
+
 	FD_ZERO(&rds);
 	FD_SET(fd, &rds);
 	FD_SET(shell->cancel_pipe[0], &rds);
-	
+
 	if (select(shell->cancel_pipe[0] + 1, &rds, NULL, NULL, NULL) > 0) {
 		if (FD_ISSET(fd, &rds)) {
 			return 1;
@@ -43,12 +43,12 @@ int command_get(shell_t *shell, command_t *command, int fd_from) {
 	char	*temp_read_buffer;
 	char 	ate = 1;
 
-	command_copy_reminder(shell, read_buffer);	
+	command_copy_reminder(shell, read_buffer);
 	count = hstrlen(read_buffer);
 	while (ate) {
 		if (command->line_size > COMMAND_GET_MAXIMUM_CMD_SIZE) {
-			return ERR_GET_COMMAND_TO_BIG; 
-		} 
+			return ERR_GET_COMMAND_TO_BIG;
+		}
 		if (count || command_wait(shell, fd_from)) {
 			if (!count) {
 				count = read(fd_from, read_buffer, LINE_BUFFER_SIZE);
@@ -62,14 +62,13 @@ int command_get(shell_t *shell, command_t *command, int fd_from) {
 			temp_read_buffer = read_buffer;
 
 			while (count-- && ate) {
-				ate = *temp_read_buffer++; 
-				if (ate == '\n' && !inhib_next) {
+				ate = *temp_read_buffer++;
+				if (!ate || ((ate == '\n') && !inhib_next)) {
 					if (count > 1) {
 						shell->command_reminder = hstrndup(temp_read_buffer, count - 1);
 					}
 					ate = 0;
-				}				
-
+				}
 				inhib_next = ate == '\\';
 				ARRAY_ADD(command->line, ate, LINE_BUFFER_SIZE);
 				if (command->line == NULL) {
