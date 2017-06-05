3caa64b57738853d04e34debaba7d9771eeb20d4
Alexandre Gautier <alexandre.gautier@epitech.eu>
2017-06-05 17:50:55 +0200

    Fix: # symbol in a middle of a word does not start a comment

--- src/command/command_remove_comment.c
+++ src/command/command_remove_comment.c
@@ -4,11 +4,13 @@ void command_remove_comment(command_chain_t *chain) {
 	char	*line = chain->line;
 	int		in = 0;
 	int		inhib = 0;
+	int		in_word = 0;
+
 	for (;*line; ++line) {
 		char at = *line;
 		switch (at) {
 			case '#': 
-				in = inhib == 0;
+				in = (inhib == 0 && in_word == 0);
 				break;
 			case '"': 
 			case '\'':
@@ -22,6 +24,14 @@ void command_remove_comment(command_chain_t *chain) {
 				break;
 			case '\n':
 				in = 0;
+				in_word = 0;
+				break;
+			case ' ':
+			case '\t':
+				in_word = 0;
+				break;
+			default:
+				in_word = 1;
 				break;
 		}
 		if (in) {
